19ec1761cb1a8417d72654641e3782e917b940ec
SimoL-003 <simoneleuzzi2003@gmail.com>
2025-10-25 14:50:35 +0200

    feat: Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import logo from "../assets/icons/dc-logo.png";
+import "./header.css";
+
+export default function Header() {
+  const menuItems = [
+    "characters",
+    "comics",
+    "movies",
+    "tv",
+    "games",
+    "collectibles",
+    "videos",
+    "fans",
+    "news",
+    "shop",
+  ];
+
+  return (
+    <header>
+      <div className="container">
+        <div className="logo">
+          <a href="/">
+            <img src={logo} alt="Logo DC" />
+          </a>
+        </div>
+        <nav>
+          <ul>
+            {menuItems.map((curItem) => (
+              <li className={curItem === "comics" ? "active" : ""}>
+                <a href={`/${curItem}`}>{curItem}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
