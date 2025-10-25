3e8db2105d558bfd5fb0bf77cd37276178fbc5fd
SimoL-003 <simoneleuzzi2003@gmail.com>
2025-10-25 16:52:02 +0200

    feat: Footer bottom

--- src/components/FooterBottom.jsx
+++ src/components/FooterBottom.jsx
@@ -1,3 +1,58 @@
+import style from "./Footer.module.css";
+import facebookIcon from "../assets/icons/footer-facebook.png";
+import twitterIcon from "../assets/icons/footer-twitter.png";
+import youtubeIcon from "../assets/icons/footer-youtube.png";
+import pinterestIcon from "../assets/icons/footer-pinterest.png";
+import periscopeIcon from "../assets/icons/footer-periscope.png";
+
 export default function FooterBottom() {
-  return <section></section>;
+  const social = [
+    {
+      name: "Facebook",
+      link: "#",
+      icon: facebookIcon,
+    },
+    {
+      name: "Twitter",
+      link: "#",
+      icon: twitterIcon,
+    },
+    {
+      name: "YouTube",
+      link: "#",
+      icon: youtubeIcon,
+    },
+    {
+      name: "Pinterest",
+      link: "#",
+      icon: pinterestIcon,
+    },
+    {
+      name: "Periscope",
+      link: "#",
+      icon: periscopeIcon,
+    },
+  ];
+
+  return (
+    <section className={style.footerBottom}>
+      <div className={`container ${style.footerBottomContainer}`}>
+        <div className={style.btn}>
+          <a href="#">Sign-up now!</a>
+        </div>
+        <div className={style.social}>
+          <h3>Follow us</h3>
+          <ul>
+            {social.map((curItem, index) => (
+              <li key={index}>
+                <a href={curItem.link}>
+                  <img src={curItem.icon} alt={curItem.title} />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 }
